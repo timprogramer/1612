44bdf74d79c96b38f9313ab7e008d9b113635257
Tim GF63 <tim@shatzir.com>
2021-12-10 18:48:23 +0900

    1012

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import random
 pepple=random.choice(["красивоя","ужасноя"])
 peppleludoed=random.choice(["голодный","не голодный"])
-wether=random.choice(["плохая ","ясноя"])
-print("погода была",wether ,"и принцесса была",pepple)
-if wether=="ясноя" and pepple=="красивоя" or pepple=="ужасноя":
+wether=random.choice(["плохая","ясноя"])
+print("погода была",wether ,"и принцесса была",pepple ,"и людоед был ",peppleludoed)
+if (wether=="ясноя" and pepple=="красивоя") or (wether=="ясноя" and pepple=="ужасноя") or (wether=="плохая" and pepple=="красивоя"):
     print("принцесса пошла гулять")
+    if (peppleludoed == "голодный" and pepple=="красивоя") or (peppleludoed=="не голодный" and pepple=="красивоя") or (peppleludoed=="голодный" and pepple=="ужасноя"):
+        print("принцессу съел людоед")
+    else:
+        print("принцессу не съел людоед")
+else:
+    print("принцесса не пошла гулять")
